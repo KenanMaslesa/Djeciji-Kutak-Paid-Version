b0d35211da942df1571d7eea166741f90571a5c5
KenanMaslesa <kenoo.maslesa@gmail.com>
2021-09-20 22:41:48 +0200

    .

--- src/app/pages/homepage-anonymous/homepage-anonymous.component.ts
+++ src/app/pages/homepage-anonymous/homepage-anonymous.component.ts
@@ -41,7 +41,7 @@ export class HomepageAnonymousComponent implements OnInit {
     this.faqs = [
       {
         question: 'Zašto se korištenje stranice/aplikacije naplaćuje?',
-        answer: 'Kako je veliki broj ljudi počeo koristiti našu stranicu/aplikaciju došli su nam veći troškovi a i zahtjevi. Odlučili smo da simbolično naplatimo korištenje naše aplikacije kako bi tim sredstvima mogli platiti troškove i unaprijediti razvoj i održavanje stranice/aplikacije.'
+        answer: 'Kako je veliki broj ljudi počeo koristiti našu stranicu/aplikaciju došli su nam veći troškovi, a tome se nismo nadali. Imamo mogućnost da zarađujemo na reklamama ali ne želimo djecu izlagati sadržaju koji ne možemo u potpunosti kontrolisati. Odlučili smo da simbolično naplatimo korištenje aplikacije jer smatramo da Vaše malo može mnogo olakšati njen razvoj i održavanje. Često dobijamo nove zahtjeve i ideje a za to su nam potrebna novčana sredstva. Mnogo vremena i truda smo uložili da napravimo aplikaciju i da svaki crtani film i pjesmicu detaljno pregledamo i izbacimo muziku i sve ono što je neprimjereno. Potrebna nam je Vaša novčana podrška kako bismo nastavili sa daljnjim radom i održavanjem.'
       },
       {
         question: 'Da li moram imati paypal da bih se pretplatio?',
