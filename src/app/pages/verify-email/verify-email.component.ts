4e4dff836d2ebf6c0557e51b6261ed128c012307
KenanMaslesa <kenoo.maslesa@gmail.com>
2021-09-20 15:39:37 +0200

    .

--- src/app/pages/verify-email/verify-email.component.ts
+++ src/app/pages/verify-email/verify-email.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
@@ -8,9 +9,12 @@ import { AuthService } from 'src/app/shared/auth.service';
 })
 export class VerifyEmailComponent implements OnInit {
 
-  constructor(public authService:AuthService) { }
+  constructor(public authService:AuthService, private router:Router) { }
 
   ngOnInit(): void {
+    if(this.authService.getCurrentUser().emailVerified && this.authService.isLoggedIn){
+      this.router.navigate(['videos']);
+    }
   }
 
 }
