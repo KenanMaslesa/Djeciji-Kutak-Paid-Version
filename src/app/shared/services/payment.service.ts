96ae5eec3423807447a10eb6dadd8ebcdb236eea
KenanMaslesa <kenoo.maslesa@gmail.com>
2021-10-22 17:39:52 +0200

    location.reload();

--- src/app/shared/services/payment.service.ts
+++ src/app/shared/services/payment.service.ts
@@ -175,7 +175,7 @@ export class PaymentService {
           this.checkPaypalSubcription(this.subscriptionID);
         }
       }, error => {
-        if(error.status == 401){
+        if(error.status == 401 || error.status == 501){
           location.reload();
         }
       });
